ba5d73793e448b1ba9fb4c91fe950381ff5fd1b6
paner28 <61655946+paner28@users.noreply.github.com>
2020-03-26 13:53:07 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@
 
 {
     function clickBtn(){
+        // フォームで先頭の値を取得
         const headNum = document.form1.num.value;
         const pop = document.querySelector('#pop')
-    
+
+        var k = 0
+        const nokori = document.createElement('div');
+        nokori.className = 'nokori';
+        nokori.id="nokori"
+        pop.appendChild(nokori)
+        // ここまで残り個数を表示するdivを作成
+        
         for(let i = 0 ; i < 10 ; i++ ){
             const parent = document.createElement('div');
             parent.className = 'parent';
@@ -20,22 +28,33 @@
                     case 0:
                         kazu += 1;
                         break;
-                    case 1:
+                        case 1:
                         kazu += 3;
                         break;
-                    case 2:
-                        kazu += 7;
+                        case 2:
+                            kazu += 7;
                         break;
-                    case 3:
+                        case 3:
                         kazu += 9;
                         break;
                 }
                 child.textContent = kazu;
+                if (primeNumber(kazu)){
+                    k++
+                }
                 
                 child.addEventListener('click',()=>{
                     if (primeNumber(kazu)){
                         child.textContent = 'Prime!';
                         child.classList.add('win');
+                        k--
+                        console.log(k)
+                        nokori.textContent = "残り"+k+"個です"
+                        flag = 1
+                        if (k===0){
+                            nokori.textContent = "Congratulations!!!"
+                            nokori.className = 'nokori0';
+                        }
                     }else{
                         child.textContent = 'No!';
                         child.classList.add('lose');
@@ -45,14 +64,15 @@
                     child.classList.add("tooltip1");
                     chip.textContent = kazu;
                     child.appendChild(chip);
-                })
-                
+                },{once:true})
                 parent.appendChild(child);
             }
-        pop.appendChild(parent);
+            pop.appendChild(parent);
+            nokori.textContent = "残り"+k+"個です"
         }
     }
     
+    // pop内の要素をすべて消去する
     function clearBtn(){
         var element = document.getElementById("pop");
         while (element.firstChild) {
@@ -60,6 +80,7 @@
         }
     }
     
+    // 素数判定
     function primeNumber (num) {
         if(num === 2) {
             return true;
@@ -76,3 +97,4 @@
         }
     }
 }
+            
